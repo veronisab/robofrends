ae5589b7f75e3cc17381c3819cb6cafbbc63a701
Veronica Ulrikkeholm <mail@veronicaisabelle.com>
2022-09-08 11:14:30 +0200

    Add files

---
+++ robofrends/src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+    id: 1,
+    name: 'Leanne Poulsen',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+},
+{
+    id: 2,
+    name: 'Sofus Jes',
+    username: 'Sofu',
+    email: 'sofuds@melis.tv'
+},
+{
+    id: 3,
+    name: 'Pil Fre',
+    username: 'pilfre',
+    email: 'pilpil@mgmail.com'
+},
+{
+    id: 4,
+    name: 'Snail Fel',
+    username: 'snaily',
+    email: 'sneila@mgmail.com'
+},
+{
+    id: 5,
+    name: 'Bjarna Nala',
+    username: 'bjarnena',
+    email: 'nalabjarne@mgmail.com'
+},
+{
+    id: 6,
+    name: 'Sofie Alden',
+    username: 'aldenso',
+    email: 'sofiealden@alden.com'
+},
+{
+    id: 7,
+    name: 'Pernille Sound',
+    username: 'sou',
+    email: 'spernille@me.com'
+},
+{
+    id: 8,
+    name: 'Selma Lager',
+    username: 'lages',
+    email: 'selma@me.com'
+},
+{
+    id: 9,
+    name: 'Jenz Bo',
+    username: 'bojenz',
+    email: 'jenza@bo.com'
+},
+{
+    id: 10,
+    name: 'Elvira Jensen',
+    username: 'elivarj',
+    email: 'elvirajensen@hotmail.com'
+}
+]
